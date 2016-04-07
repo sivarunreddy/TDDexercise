f1736b8c90cc46964b28e3a27786be43a11000fe
sivarunreddy <sambasiva.patti@target.com>
2016-04-07 12:18:53 +0530

    Added logic to support two digits

--- SpellItOut1/src/com/tdd/exercise/converter/NumberConverter.java
+++ SpellItOut1/src/com/tdd/exercise/converter/NumberConverter.java
@@ -2,27 +2,39 @@ package com.tdd.exercise.converter;
 
 public class NumberConverter {
 	private Double number;
-	private static final String UNITS[]=new String[]{"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
-	private static final String TEENS[]=new String[]{"Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+	private static final String UNITS[] = new String[] { "Zero", "One", "Two",
+			"Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+	private static final String TEENS[] = new String[] { "Ten", "Eleven",
+			"Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+			"Seventeen", "Eighteen", "Nineteen" };
+	private static final String TENS[] = new String[] { "Twenty", "Thirty",
+			"Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
 	public NumberConverter(Double number) {
 		this.number = number;
 	}
 
 	public String convertToWords() {
-		Long wholeNumber=number.longValue();
-		if(number<10){
-			return getUnitName(wholeNumber.intValue());
-		}else{
-			return getTeenName(wholeNumber.intValue());
-		}
+		Long wholeNumber = number.longValue();
+		if (number < 10) {
+			return getUnitName(wholeNumber);
+		} else if(number<20){
+			return getTeenName(wholeNumber);
+		}else 
+			return getTenName(wholeNumber)+(wholeNumber%10==0?"":" "+getUnitName(wholeNumber%10));
+		
+	}
+
+	private String getUnitName(Long number) {
+		return UNITS[number.intValue()];
+	}
+
+	private String getTeenName(Long number) {
+		return TEENS[number.intValue() % 10];
 	}
 	
-	private String getUnitName(int number){
-		return UNITS[number]; 
-	}
-	
-	private String getTeenName(int number){
-		return TEENS[number%10];
+	private String getTenName(Long number){
+		return TENS[(number.intValue()/10)-2];
 	}
 
 }
--- SpellItOut1/test/com/tdd/exercise/converter/NumberConverterTest.java
+++ SpellItOut1/test/com/tdd/exercise/converter/NumberConverterTest.java
@@ -11,41 +11,77 @@ public class NumberConverterTest {
 		String digit = new NumberConverter(new Double(1)).convertToWords();
 		assertThat(digit, is("One"));
 	}
-	
+
 	@Test
 	public void test_convert2AsTwo() throws Exception {
 		String digit = new NumberConverter(new Double(2)).convertToWords();
 		assertThat(digit, is("Two"));
 	}
-	
+
 	@Test
 	public void test_convert9AsNine() throws Exception {
 		String digit = new NumberConverter(new Double(9)).convertToWords();
 		assertThat(digit, is("Nine"));
 	}
-	
+
 	@Test
 	public void test_convert10AsTen() throws Exception {
 		String digit = new NumberConverter(new Double(10)).convertToWords();
 		assertThat(digit, is("Ten"));
 	}
-	
+
 	@Test
 	public void test_convert11AsElevan() throws Exception {
 		String digit = new NumberConverter(new Double(11)).convertToWords();
 		assertThat(digit, is("Eleven"));
 	}
-	
+
 	@Test
 	public void test_convert15AsFifteen() throws Exception {
 		String digit = new NumberConverter(new Double(15)).convertToWords();
 		assertThat(digit, is("Fifteen"));
 	}
-	
+
 	@Test
 	public void test_convert19AsNineteen() throws Exception {
 		String digit = new NumberConverter(new Double(19)).convertToWords();
 		assertThat(digit, is("Nineteen"));
+	}
+
+	@Test
+	public void test_convert20AsTwenty() throws Exception {
+		String digit = new NumberConverter(new Double(20)).convertToWords();
+		assertThat(digit, is("Twenty"));
+	}
+	
+	@Test
+	public void test_convert21AsTwentyOne() throws Exception {
+		String digit = new NumberConverter(new Double(21)).convertToWords();
+		assertThat(digit, is("Twenty One"));
+	}
+	
+	@Test
+	public void test_convert29AsTwentyNine() throws Exception {
+		String digit = new NumberConverter(new Double(29)).convertToWords();
+		assertThat(digit, is("Twenty Nine"));
+	}
+	
+	@Test
+	public void test_convert30AsThirty() throws Exception {
+		String digit = new NumberConverter(new Double(30)).convertToWords();
+		assertThat(digit, is("Thirty"));
+	}
+	
+	@Test
+	public void test_convert50AsFifty() throws Exception {
+		String digit = new NumberConverter(new Double(50)).convertToWords();
+		assertThat(digit, is("Fifty"));
+	}
+	
+	@Test
+	public void test_convert99AsNinetyNine() throws Exception {
+		String digit = new NumberConverter(new Double(99)).convertToWords();
+		assertThat(digit, is("Ninety Nine"));
 	}
 
 }
