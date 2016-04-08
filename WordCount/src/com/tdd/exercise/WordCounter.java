a4af75bdc03f49f9d4653be5ef2963e653cb2415
sivarunreddy <sambasiva.patti@target.com>
2016-04-08 15:21:54 +0530

    Code refactor to make it simple.

--- WordCount/src/com/tdd/exercise/WordCounter.java
+++ WordCount/src/com/tdd/exercise/WordCounter.java
@@ -1,32 +1,29 @@
 package com.tdd.exercise;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WordCounter {
 
 	private String text;
-	private static final String DEFAULT_DELIMITOR=" ";
+	private static final String DEFAULT_DELIMITOR = " ";
 
 	public WordCounter(String text) {
 		this.text = text;
 	}
 
 	public Map<String, Integer> getUniqueWordsCount() {
-		Map<String,Integer> wordMap=new HashMap<>();
-		if(!text.isEmpty()){
-			String[] words=text.split(DEFAULT_DELIMITOR);
-			for(String word:words){
-				Integer counter=wordMap.get(word);
-				if(null==counter)
-					counter=1;
-				else
-					counter++;
-				wordMap.put(word, counter);
-			}
-		}
-			
-		return wordMap;
+		return getWordsFrequency(text);
 	}
 
+	private Map<String, Integer> getWordsFrequency(String text) {
+		Map<String, Integer> wordMap = new HashMap<>();
+		Arrays.stream(text.split(DEFAULT_DELIMITOR)).forEach(word -> {
+			Integer counter = wordMap.get(word);
+			wordMap.put(word, counter == null ? 1 : ++counter);
+		});
+
+		return wordMap;
+	}
 }
